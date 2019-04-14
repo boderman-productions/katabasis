9207084c13d3680d90f40d1b85b7a55622d81944
cliff-jam <cliff.jam.au@gmail.com>
2019-04-14 23:12:08 +1000

    Stanky terrain

---
+++ Assets/Scripts/PerlinTerrainGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PerlinTerrainGenerator : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+    public int depth = 10;
+    public float scale = 5f;
+
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData); //terrain data = newly generated terrain, and not the default setting
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights ()
+    {
+        float[,] heights = new float[width, height]; //grid of points
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                heights[x, y] = CalculateHeight(x, y); //calculates height at the iterated x and y coord
+            }
+        }
+        return heights;
+    }
+
+    float CalculateHeight (int x, int y) //converts coords into noisemap coords
+    {
+        float xCoord = (float)x / width * scale;
+        float yCoord = (float)y / height * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord); 
+    }
+}
