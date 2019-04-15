75fe2898f3d586ffa93997e373e6a1c44cb23823
cliff-jam <cliff.jam.au@gmail.com>
2019-04-16 09:02:12 +1000

    Rain generator

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -9,8 +9,7 @@ public class FaceCamera : MonoBehaviour
     //Orient the camera after all movement is completed this frame to avoid jittering
     void LateUpdate()
     {
-        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
-            m_Camera.transform.rotation * Vector3.up);
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward, m_Camera.transform.rotation * Vector3.up);
     }
 }
 
---
+++ Assets/Scripts/Weather/WeatherSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherSystem : MonoBehaviour
+{
+    [System.Serializable] //exposes rain values to editor
+    public class rainEditor
+    {
+        public int rainAmount;
+    }
+
+    public GameObject Rain;
+    public Vector3 terrainCoord;
+    public int rainX;
+    public int rainY;
+
+    void Start()
+    {
+        Vector3 terrainCoord = GameObject.Find("BaseTerrain").GetComponent<Terrain>().terrainData.size; //gets the dimensions of the base terrain layer to ensure nothing falls outside
+        Debug.Log(terrainCoord);
+    }
+
+    void Update()
+    {
+        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth); 
+        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+    }
+}
