eec90ea63eb2314c4ac99bd76e4b551740dd9d34
cliff-jam <cliff.jam.au@gmail.com>
2019-04-17 08:40:24 +1000

    Tied waterlevel to amount of rain

--- Assets/Scripts/Weather/WeatherSystem.cs
+++ Assets/Scripts/Weather/WeatherSystem.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class WeatherSystem : MonoBehaviour
 {
-    [System.Serializable] //exposes rain values to editor
-    public class rainEditor
-    {
-        public int rainAmount;
-    }
-
+    public static float minWaterLevel;
+    public static float maxWaterLevel;
+    public static bool raining;
     public GameObject Rain;
     public Vector3 terrainCoord;
     private int rainX;
@@ -16,26 +13,31 @@ public class WeatherSystem : MonoBehaviour
 
     void Start()
     {
+        minWaterLevel = 1f;
+        maxWaterLevel = 15f;
+        raining = true;
         Vector3 terrainCoord = GameObject.Find("BaseTerrain").GetComponent<Terrain>().terrainData.size; //gets the dimensions of the base terrain layer to ensure nothing falls outside
-        Debug.Log(terrainCoord);
     }
 
     void Update()
     {
-        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth); 
-        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
-        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
-        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
-        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
-        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
-        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
-        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
-        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
-        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
-        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
-        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
-        rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
-        rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
-        Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+        if (raining = true) //standard rain
+        {
+            rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+            rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+            Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+            rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+            rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+            Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+            rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+            rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+            Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+            rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+            rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+            Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+            rainX = Random.Range(0, PerlinTerrainGenerator.terrainWidth);
+            rainY = Random.Range(0, PerlinTerrainGenerator.terrainLength);
+            Instantiate(Rain, new Vector3(rainX, 100, rainY), Quaternion.identity); //instantiates random droplet at random coordinates above terrain map
+        }
     }
 }
